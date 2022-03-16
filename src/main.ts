5bbdb839efd40ad7bb5b317bc351f21da70f036b
CorneliaBz <bergentz2@hotmail.com>
2022-03-16 11:58:54 +0100

    en tredje bild

--- src/main.ts
+++ src/main.ts
@@ -8,13 +8,14 @@ const img = document.createElement("img");
 img.src = imgUrl.href; //ger oss en string med hela url:en till bilden
 document.body.append(img);
 
-
-
-
-
 const imgUrl2 = new URL('img/bild2.jpg', import.meta.url);
 const img2 = document.createElement("img");
 document.body.append(img2);
 
 img2.src = imgUrl2.href; //ger oss en string med hela url:en till bilden
 
+const imgUrl3 = new URL('img/sal.jpg', import.meta.url);
+const img3 = document.createElement("img");
+document.body.append(img3);
+
+img3.src = imgUrl3.href; //ger oss en string med hela url:en till bilden
